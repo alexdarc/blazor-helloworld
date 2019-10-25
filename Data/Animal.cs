8f14d8343f3f98a2cfcee9e56a0a74595af27d1d
Alexander Kim <alex.dark.kim@gmail.com>
2019-10-25 22:54:18 +0300

    init

---
+++ Data/Animal.cs
@@ -0,0 +1,17 @@
+namespace WebApplicationServer1.Data
+{
+    public class Animal
+    {
+        public Animal(
+            string name,
+            string imageUrl)
+        {
+            this.Name = name;
+            this.ImageUrl = imageUrl;
+        }
+
+        public string Name { get; }
+
+        public string ImageUrl { get; }
+    }
+}
